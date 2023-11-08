39ec914660a03b3cf757258a250611d7baa2c850
Martina <a.martina.eriksson@hotmail.com>
2023-11-08 14:29:39 +0100

    adds fields and constructor to product class

--- Week2Project/Product.cs
+++ Week2Project/Product.cs
@@ -8,5 +8,15 @@ namespace Week2Project
 {
     internal class Product
     {
+        public Product(string category, string name, int price)
+        {
+            Category = category;
+            Name = name;
+            Price = price;
+        }
+
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
     }
 }
