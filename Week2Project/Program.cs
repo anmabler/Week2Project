6e1ab380456c57556005760dc94ecda7489d8273
Martina <a.martina.eriksson@hotmail.com>
2023-11-10 10:09:55 +0100

    change: moves code for displaying list to function

--- Week2Project/Program.cs
+++ Week2Project/Program.cs
@@ -1,59 +1,13 @@
 ﻿using Week2Project;
 
 List<Product> products = new List<Product>();
-products.AddRange(addProducts());
-//Console.WriteLine("Enter a new product. Enter 'q' to quit");
-//while (true)
-//{
-//    Console.Write("Enter Category: ");
-//    string categoryInput = Console.ReadLine();
-//    if (categoryInput.ToLower().Trim() == "q")
-//    {
-//        break;
-//    }
 
-//    Console.Write("Enter Product: ");
-//    string productInput = Console.ReadLine();
-//    if (productInput.ToLower().Trim() == "q")
-//    {
-//        break;
-//    }
+addProducts(products);
+displayList(products);
 
-//    Console.Write("Enter Price: ");
-//    string priceInput = Console.ReadLine();
-//    if(priceInput.ToLower().Trim() == "q")
-//    {
-//        break;
-//    }
 
-//    bool isInt = int.TryParse(priceInput, out int value);
-//    if (isInt)
-//    {
-//        Product product = new Product(categoryInput, productInput, value);
-//        products.Add(product);
-//        Console.ForegroundColor = ConsoleColor.Green;
-//        Console.WriteLine("The product was added.");
-//        Console.ResetColor();
-//    }
-
-//}
-
-Console.WriteLine("Your product list: ");
-Console.WriteLine("Category".PadRight(15) + "Name".PadRight(15) + "Price");
-// ascending
-var sortedProductList = products.OrderBy(product => product.Price).ToList();
-foreach (Product product in sortedProductList)
+static List<Product> addProducts (List<Product> products)
 {
-    Console.WriteLine(product.Category.PadRight(15) + product.Name.PadRight(15) + product.Price);
-}
-Console.WriteLine("-----------------------------------");
-int priceSum = products.Sum(product => product.Price); 
-
-Console.WriteLine("\nSum ".PadRight(30) + priceSum);
-
-static List<Product> addProducts ()
-{
-    List<Product> tempProducts = new List<Product>();
     Console.WriteLine("Enter a new product. Enter 'q' to quit");
     while (true)
     {
@@ -82,12 +36,28 @@ static List<Product> addProducts ()
         if (isInt)
         {
             Product product = new Product(categoryInput, productInput, value);
-            tempProducts.Add(product);
+            products.Add(product);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("The product was added.");
             Console.ResetColor();
         }
 
     }
-    return tempProducts;
+    return products;
+}
+
+static void displayList(List<Product> products)
+{
+    Console.WriteLine("Your product list: ");
+    Console.WriteLine("Category".PadRight(15) + "Name".PadRight(15) + "Price");
+    // ascending
+    var sortedProductList = products.OrderBy(product => product.Price).ToList();
+    foreach (Product product in sortedProductList)
+    {
+        Console.WriteLine(product.Category.PadRight(15) + product.Name.PadRight(15) + product.Price);
+    }
+    Console.WriteLine("-----------------------------------");
+    int priceSum = products.Sum(product => product.Price);
+
+    Console.WriteLine("\nSum ".PadRight(30) + priceSum);
 }
